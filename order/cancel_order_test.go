bb2bcf1d4ec9bef35dca67e5c7cdae8de4e77356
conni <cbuschka@gmail.com>
2020-07-18 14:41:35 +0200

    Added more cancel tests.

--- order/cancel_order_test.go
+++ order/cancel_order_test.go
@@ -17,3 +17,25 @@ func TestCanCancelOrderWhenPlaced(t *testing.T) {
     assert.Equal(t, cmd.OrderNumber, order.OrderNumber)
     assert.Equal(t, OrderCancelled, order.OrderStatus)
 }
+
+func TestCannotCancelOrderWhenCancelled(t *testing.T) {
+    order := NewOrder()
+    order.OrderStatus = OrderCancelled
+    order.OrderNumber = "O1"
+
+    cmd := CancelOrderCommand{OrderNumber: order.OrderNumber}
+    err := order.CancelOrder(&cmd)
+
+    assert.NotNil(t, err)
+}
+
+func TestCannotCancelOrderWhenNew(t *testing.T) {
+    order := NewOrder()
+    order.OrderStatus = ""
+    order.OrderNumber = "O1"
+
+    cmd := CancelOrderCommand{OrderNumber: order.OrderNumber}
+    err := order.CancelOrder(&cmd)
+
+    assert.NotNil(t, err)
+}
